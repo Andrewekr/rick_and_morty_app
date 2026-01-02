98f3d0d0ec37276f4b1d7e30d88a54bb23967516
Andrew Rubtsou <Andrei.Rubtsou@gmail.com>
2026-01-02 12:59:29 +0300

    add folders characters and favorites

---
+++ lib/features/characters/presentation/characters_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CharactersPage extends StatelessWidget {
+  const CharactersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Characters Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
